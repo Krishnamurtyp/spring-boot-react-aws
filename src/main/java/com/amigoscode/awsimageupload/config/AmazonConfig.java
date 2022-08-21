a601519aefb7c6ea02fa767f42a30128bd4249e9
ohanke <steamoha@wp.pl>
2022-08-21 22:22:05 +0200

    Amazon s3 client

---
+++ src/main/java/com/amigoscode/awsimageupload/config/AmazonConfig.java
@@ -0,0 +1,24 @@
+package com.amigoscode.awsimageupload.config;
+
+import com.amazonaws.auth.AWSCredentials;
+import com.amazonaws.auth.AWSStaticCredentialsProvider;
+import com.amazonaws.auth.BasicAWSCredentials;
+import com.amazonaws.services.s3.AmazonS3;
+import com.amazonaws.services.s3.AmazonS3ClientBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class AmazonConfig {
+
+    @Bean
+    public AmazonS3 s3(){
+        AWSCredentials awsCredentials = new BasicAWSCredentials(
+                "AKIA2CB7BWRH5X2CXF4C", "oryeGkMyDiG2+5BCY5mPqtIgYgftddeqLNJPcPys");
+
+        return AmazonS3ClientBuilder
+                .standard()
+                .withCredentials(new AWSStaticCredentialsProvider(awsCredentials))
+                .build();
+    }
+}
