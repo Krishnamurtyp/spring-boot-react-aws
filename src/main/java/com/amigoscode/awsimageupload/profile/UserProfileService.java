86dc0b5039a9ce8a52eb39812ff98ea18dbdabde
ohanke <steamoha@wp.pl>
2022-08-22 09:59:35 +0200

    upload image api

--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
@@ -1,8 +1,10 @@
 package com.amigoscode.awsimageupload.profile;
 
 import org.springframework.stereotype.Service;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
+import java.util.UUID;
 
 @Service
 public class UserProfileService {
@@ -15,5 +17,9 @@ public class UserProfileService {
 
     List<UserProfile> getUserProfiles(){
         return userProfileDataAccessService.getUserProfiles();
+    }
+
+    public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
+
     }
 }
