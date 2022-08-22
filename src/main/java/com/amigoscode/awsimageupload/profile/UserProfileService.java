97e04cd195f339d6cf4d4004e450ad3bb66980d1
ohanke <steamoha@wp.pl>
2022-08-22 14:32:20 +0200

    uploadUserProfileImage()

--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
@@ -1,18 +1,23 @@
 package com.amigoscode.awsimageupload.profile;
 
+import com.amigoscode.awsimageupload.bucket.BucketName;
+import com.amigoscode.awsimageupload.filesStrore.FileStore;
+import org.apache.http.entity.ContentType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
-import java.util.List;
-import java.util.UUID;
+import java.io.IOException;
+import java.util.*;
 
 @Service
 public class UserProfileService {
 
     private final UserProfileDataAccessService userProfileDataAccessService;
+    private final FileStore fileStore;
 
-    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService) {
+    public UserProfileService(UserProfileDataAccessService userProfileDataAccessService, FileStore fileStore) {
         this.userProfileDataAccessService = userProfileDataAccessService;
+        this.fileStore = fileStore;
     }
 
     List<UserProfile> getUserProfiles(){
@@ -21,9 +26,30 @@ public class UserProfileService {
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         //1. Check if image is not empty
+        if  (file.isEmpty()){
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
+        }
         //2. If file is an image
+        if (Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF)
+                .contains(file.getContentType())){
+            throw new IllegalStateException("File must be an image");
+        }
         //3. The user exists in our database
+        UserProfile user = userProfileDataAccessService.getUserProfiles().stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
         //4. Grab some metadata from file if any
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
         //5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
+        String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
+        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        try {
+            fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
+        } catch (IOException e) {
+            throw new IllegalStateException(e);
+        }
     }
 }
