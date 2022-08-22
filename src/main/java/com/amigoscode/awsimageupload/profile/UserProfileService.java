5f0707bbe42e3674befc3d6f18978ee285a2971a
ohanke <steamoha@wp.pl>
2022-08-22 14:51:36 +0200

    .

--- src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
+++ src/main/java/com/amigoscode/awsimageupload/profile/UserProfileService.java
@@ -26,30 +26,49 @@ public class UserProfileService {
 
     public void uploadUserProfileImage(UUID userProfileId, MultipartFile file) {
         //1. Check if image is not empty
-        if  (file.isEmpty()){
-            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
-        }
+        isFileEmpty(file);
         //2. If file is an image
-        if (Arrays.asList(ContentType.IMAGE_JPEG, ContentType.IMAGE_PNG, ContentType.IMAGE_GIF)
-                .contains(file.getContentType())){
-            throw new IllegalStateException("File must be an image");
-        }
+        isImage(file);
         //3. The user exists in our database
-        UserProfile user = userProfileDataAccessService.getUserProfiles().stream()
-                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
-                .findFirst()
-                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+        UserProfile user = getUserProfileOrThrow(userProfileId);
         //4. Grab some metadata from file if any
-        Map<String, String> metadata = new HashMap<>();
-        metadata.put("Content-Type", file.getContentType());
-        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        Map<String, String> metadata = extractMetadata(file);
         //5. Store the image in s3 and update database (userProfileImageLink) with s3 image link
         String path = String.format("%s/%s", BucketName.PROFILE_IMAGE.getBucketName(), user.getUserProfileId());
-        String fileName = String.format("%s-%s", file.getName(), UUID.randomUUID());
+        String fileName = String.format("%s-%s", file.getOriginalFilename(), UUID.randomUUID());
         try {
             fileStore.save(path, fileName, Optional.of(metadata), file.getInputStream());
         } catch (IOException e) {
             throw new IllegalStateException(e);
+        }
+    }
+
+    private static Map<String, String> extractMetadata(MultipartFile file) {
+        Map<String, String> metadata = new HashMap<>();
+        metadata.put("Content-Type", file.getContentType());
+        metadata.put("Content-Length", String.valueOf(file.getSize()));
+        return metadata;
+    }
+
+    private UserProfile getUserProfileOrThrow(UUID userProfileId) {
+        return userProfileDataAccessService.getUserProfiles().stream()
+                .filter(userProfile -> userProfile.getUserProfileId().equals(userProfileId))
+                .findFirst()
+                .orElseThrow(() -> new IllegalStateException(String.format("User profile %s not found", userProfileId)));
+    }
+
+    private static void isImage(MultipartFile file) {
+        if (Arrays.asList(ContentType.IMAGE_JPEG.getMimeType(),
+                        ContentType.IMAGE_PNG.getMimeType(),
+                        ContentType.IMAGE_GIF.getMimeType()).contains(file.getContentType())){
+
+            throw new IllegalStateException("File must be an image [" + file.getContentType() + "]");
+        }
+    }
+
+    private static void isFileEmpty(MultipartFile file) {
+        if  (file.isEmpty()){
+            throw new IllegalStateException("Cannot upload empty file [" + file.getSize() + "]");
         }
     }
 }
